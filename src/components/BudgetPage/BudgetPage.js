bb0142fc50c40b533a1900ac985a1c3dab194f27
Kenneth Chow <kennethjchow@gmail.com>
2021-03-07 17:19:38 -0800

    Added header for current week

--- src/components/BudgetPage/BudgetPage.js
+++ src/components/BudgetPage/BudgetPage.js
@@ -1,19 +1,21 @@
 import { Component } from "react";
 import Sections from "../Sections/Sections";
 import axios from "axios";
+import moment from "moment";
 import "./BudgetPage.css";
 import AddTimeButton from "../AddTimeButton/AddTimeButton";
 
 class BudgetPage extends Component {
    constructor(props) {
-      super(props)
-  
-      this.addTimeHandler = this.addTimeHandler.bind(this)
-    }
-   
+      super(props);
+
+      this.addTimeHandler = this.addTimeHandler.bind(this);
+   }
+
    state = {
       budgetCategories: [],
       loaded: false,
+      hoursLeft: 172,
    };
 
    componentDidMount() {
@@ -27,16 +29,27 @@ class BudgetPage extends Component {
    }
 
    addTimeHandler() {
-      let budgetCategoriesCopy = [...this.state.budgetCategories]
-      budgetCategoriesCopy[0].time_spent+=1
-      this.setState({budgetCategories: budgetCategoriesCopy});
+      let budgetCategoriesCopy = [...this.state.budgetCategories];
+      budgetCategoriesCopy[0].time_spent += 1;
+      this.setState({ budgetCategories: budgetCategoriesCopy });
    }
-
    render() {
+      const startOfWeek = moment().startOf('isoweek').format('MMMM Do')
+      const endOfWeek = moment().endOf("isoweek").format("MMMM Do")
+      const year = moment().startOf('isoweek').format('YYYY')
       return (
          <div className="budgetPage">
+            <div className="header">
+               <h5 className="headerYear">
+                  {year}
+               </h5>
+               <h1 className="headerDate">
+                  {startOfWeek} - {endOfWeek}
+               </h1>
+               <div>{this.state.hoursLeft} hours left to plan</div>
+            </div>
             <Sections budgets={this.state.budgetCategories}></Sections>
-				<AddTimeButton addTimeHandler={this.addTimeHandler}></AddTimeButton>
+            <AddTimeButton addTimeHandler={this.addTimeHandler}></AddTimeButton>
          </div>
       );
    }
