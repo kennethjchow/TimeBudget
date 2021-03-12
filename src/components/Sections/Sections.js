25f8b48c62a874e127d87c0f44d3030240fe616b
Kenneth Chow <kennethjchow@gmail.com>
2021-03-11 19:41:15 -0800

    Now can add item

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -2,31 +2,52 @@ import "./Sections.css";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import React from "react";
+import { Component } from "react";
 
-function Section(props) {
-   const budgets = props.budgets;
-   const listBudgetGroups = budgets.map((budget) => (
-      <div className="budget-group">
-         <div className="budget-group-header budget-item-input-group">
-            <InputGroup
-               className="budget-group-header-name"
-            >
-               <FormControl
-                  className="budget-item-form-control"
-                  placeholder="Item Name"
-                  aria-label="Small"
-                  aria-describedby="inputGroup-sizing-sm"
-                  defaultValue={budget.group_name}
-               />
-            </InputGroup>
-            
-            <span className="budget-group-header-column">Budgeted</span>
-            <span className="budget-group-header-column">Spent</span>
+class Section extends Component {
+   constructor(props) {
+      super(props);
+      
+      this.addItem = this.addItem.bind(this);
+   }
+
+   state = {budgets: this.props.budgets}
+   
+
+   addItem(index, budgetItems) {
+      console.log(index)
+      budgetItems.push({item_name: "TESTING", time_budgeted: 56, time_spent: 3})
+      const budgets = this.state.budgets;
+      budgets[index]['items'] = budgetItems
+      this.setState({budgets: budgets})
+   }
+
+   render() {
+      const listBudgetGroups = this.state.budgets.map((budget, index) => (
+         <div className="budget-group">
+            <div className="budget-group-header budget-item-input-group">
+               <InputGroup className="budget-group-header-name">
+                  <FormControl
+                     className="budget-item-form-control"
+                     placeholder="Item Name"
+                     aria-label="Small"
+                     aria-describedby="inputGroup-sizing-sm"
+                     defaultValue={budget.group_name}
+                  />
+               </InputGroup>
+
+               <span className="budget-group-header-column">Budgeted</span>
+               <span className="budget-group-header-column">Spent</span>
+            </div>
+            <ListItem items={budget.items}></ListItem>
+            <div onClick={this.addItem.bind(this, index, budget.items)}>
+               Add item
+            </div>
          </div>
-         <ListItem items={budget.items}></ListItem>
-      </div>
-   ));
-   return <div>{listBudgetGroups}</div>;
+      ));
+      return <div>{listBudgetGroups}</div>;
+   }
 }
 
 function ListItem(props) {
@@ -71,15 +92,18 @@ function ListItem(props) {
                />
             </InputGroup>
          </div>
-         <ProgressBar className="budget-item-progress" now={calculatePercentDiff(item.time_budgeted, item.time_spent)} />
+         <ProgressBar
+            className="budget-item-progress"
+            now={calculatePercentDiff(item.time_budgeted, item.time_spent)}
+         />
       </div>
    ));
    return <div>{listBudgetItems}</div>;
 }
 
-function calculatePercentDiff (max, current) {
-   const value = Math.abs((max-current)/ max * 100 - 100)
-   return value
+function calculatePercentDiff(max, current) {
+   const value = Math.abs(((max - current) / max) * 100 - 100);
+   return value;
 }
 
 export default Section;
