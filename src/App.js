28ca8d3fa3737491cde70aa4cad3c4864fd3e997
Kenneth Chow <kennethjchow@gmail.com>
2021-03-20 17:23:09 -0700

    Added sidenav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import BudgetPage from './components/BudgetPage/BudgetPage';
+import SideNavbar from './components/SideNavbar/SideNavbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <SideNavbar></SideNavbar>
       <BudgetPage></BudgetPage>
     </div>
   );
---
+++ src/components/SideNavbar/SideNavbar.js
@@ -0,0 +1,90 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import MailIcon from "@material-ui/icons/Mail";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+   drawer: {
+      [theme.breakpoints.up("sm")]: {
+         width: drawerWidth,
+         flexShrink: 0,
+      },
+   },
+   // necessary for content to be below app bar
+   toolbar: theme.mixins.toolbar,
+   drawerPaper: {
+      width: drawerWidth,
+   },
+   content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+   },
+}));
+
+function SideNavbar(props) {
+   const classes = useStyles();
+
+   const drawer = (
+      <div>
+         <div className={classes.toolbar} />
+         <Divider />
+         <List>
+            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+               <ListItem button key={text}>
+                  <ListItemIcon>
+                     {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+               </ListItem>
+            ))}
+         </List>
+         <Divider />
+         <List>
+            {["All mail", "Trash", "Spam"].map((text, index) => (
+               <ListItem button key={text}>
+                  <ListItemIcon>
+                     {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+               </ListItem>
+            ))}
+         </List>
+      </div>
+   );
+
+   return (
+      <div>
+         <nav className={classes.drawer} aria-label="mailbox folders">
+            <Drawer
+               classes={{
+                  paper: classes.drawerPaper,
+               }}
+               variant="permanent"
+               open
+            >
+               {drawer}
+            </Drawer>
+         </nav>
+      </div>
+   );
+}
+
+SideNavbar.propTypes = {
+   /**
+    * Injected by the documentation to work in an iframe.
+    * You won't need it on your project.
+    */
+   window: PropTypes.func,
+};
+
+export default SideNavbar;
